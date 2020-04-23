c6d51437f4664cdfd78048658f4dac42fb5caea6
Christa Gammage <crgammage@gmail.com>
2020-04-23 15:16:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -19,14 +19,8 @@ def update_status
          ]
       }
    }
-
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
-
-
-
-
-  # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
+   :epic_tragedy[:capulet][:heroine][:status] = "dead"
+   :epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy
 end
 
@@ -59,29 +53,7 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus"
    #   - The second hash should have the :name key set to "Apothecary"
    # Code your solution here:
-def update_status
-  :montague => {
-     :patriarch => {name: "Lord Montague", age: "53"},
-     :matriarch => {name: "Lady Montague", age: "54"},
-     :hero => {name: "Romeo", age: "15", status: "alive"},
-     :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-     ]
-  },
-  :capulet => {
-     :patriarch => {name: "Lord Capulet", age: "50"},
-     :matriarch => {name: "Lady Capulet", age: "51"},
-     :heroine => {name: "Juliet", age: "15", status: "alive"},
-     :heroine_friends => [
-     {name: "Steven", age: "30", attitude: "confused"},
-     {name: "Nurse", age: "44", attitude: "worried"}
-     ]
-  }
-}
-  :epic_tragedy[:capulet][:heroine][:status] = "dead"
-  :epic_tragedy[:montague][:hero][:status] = "dead"
-end
+
 
 def fix_roles
   epic_tragedy[:additional_characters] = Array.new
